d4949ff58d2e1d2d1d83de75c18f07a49e8173b0
Matthew Bradbury <matt-bradbury@live.co.uk>
2019-12-12 12:15:36 +0000

    Use java.util.PriorityQueue to implement the EventQueue

--- java/org/contikios/cooja/Simulation.java
+++ java/org/contikios/cooja/Simulation.java
@@ -178,6 +178,14 @@ public class Simulation extends Observable implements Runnable {
   }
 
   /**
+   * @return True iff current thread is the simulation thread,
+   * or the simulation threat has not yet been created.
+   */
+  public boolean isSimulationThreadOrNull() {
+    return simulationThread == Thread.currentThread() || simulationThread == null;
+  }
+
+  /**
    * Schedule simulation event for given time.
    * Already scheduled events must be removed before they are rescheduled.
    *
@@ -196,7 +204,7 @@ public class Simulation extends Observable implements Runnable {
     eventQueue.addEvent(e, time);
   }
 
-  private TimeEvent delayEvent = new TimeEvent(0) {
+  private final TimeEvent delayEvent = new TimeEvent() {
     public void execute(long t) {
       if (speedLimitNone) {
         /* As fast as possible: no need to reschedule delay event */
@@ -230,7 +238,7 @@ public class Simulation extends Observable implements Runnable {
     }
   };
 
-  private TimeEvent millisecondEvent = new TimeEvent(0) {
+  private final TimeEvent millisecondEvent = new TimeEvent() {
     public void execute(long t) {
       if (!hasMillisecondObservers) {
         return;
@@ -245,7 +253,7 @@ public class Simulation extends Observable implements Runnable {
   };
 
   public void clearEvents() {
-    eventQueue.removeAll();
+    eventQueue.clear();
     pollRequests.clear();
   }
 
@@ -261,7 +269,7 @@ public class Simulation extends Observable implements Runnable {
     this.setChanged();
     this.notifyObservers(this);
 
-    TimeEvent nextEvent = null;
+    EventQueue.Pair nextEvent = null;
     try {
       while (isRunning) {
 
@@ -280,7 +288,7 @@ public class Simulation extends Observable implements Runnable {
         }
         currentSimulationTime = nextEvent.time;
         /*logger.info("Executing event #" + EVENT_COUNTER++ + " @ " + currentSimulationTime + ": " + nextEvent);*/
-        nextEvent.execute(currentSimulationTime);
+        nextEvent.event.execute(currentSimulationTime);
 
         if (stopSimulation) {
           isRunning = false;
@@ -298,8 +306,8 @@ public class Simulation extends Observable implements Runnable {
     			System.exit(1);
     		} else {
     		  String title = "Simulation error";
-    		  if (nextEvent instanceof MoteTimeEvent) {
-    		    title += ": " + ((MoteTimeEvent)nextEvent).getMote();
+    		  if (nextEvent.event instanceof MoteTimeEvent) {
+    		    title += ": " + ((MoteTimeEvent)nextEvent.event).getMote();
     		  }
     		  Cooja.showErrorDialog(Cooja.getTopParentContainer(), title, e, false);
     		}
@@ -391,7 +399,7 @@ public class Simulation extends Observable implements Runnable {
     if (isRunning()) {
       return;
     }
-    TimeEvent stopEvent = new TimeEvent(0) {
+    TimeEvent stopEvent = new TimeEvent() {
       public void execute(long t) {
         /* Stop simulation */
         stopSimulation();
@@ -788,17 +796,10 @@ public class Simulation extends Observable implements Runnable {
         setChanged();
         notifyObservers(mote);
 
-        /* Loop through all scheduled events.
-         * Delete all events associated with deleted mote. */
-        TimeEvent ev = eventQueue.peekFirst();
-        while (ev != null) {
-          if (ev instanceof MoteTimeEvent) {
-            if (((MoteTimeEvent)ev).getMote() == mote) {
-              ev.remove();
-            }
-          }
-          ev = ev.nextEvent;
-        }
+        // Delete all events associated with deleted mote.
+        eventQueue.removeIf(
+          (TimeEvent ev) ->
+            ev instanceof MoteTimeEvent && ((MoteTimeEvent)ev).getMote() == mote);
       }
     };
 
@@ -1156,7 +1157,7 @@ public class Simulation extends Observable implements Runnable {
    * @return True if simulation is runnable
    */
   public boolean isRunnable() {
-    return isRunning || hasPollRequests || eventQueue.peekFirst() != null;
+    return isRunning || hasPollRequests || !eventQueue.isEmpty();
   }
 
   /**
